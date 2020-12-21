ed912699c99b045212d521a2037b4b5b766c41b2
--replace-all <itsimmi5@gmail.com>
2020-12-20 19:34:57 -0500

    Add: using variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+#declaring and using variables
+
+# adding 2 numbers
+a = 3
+b = 5
+print(a + b)
+
+str = "hello"
+num = 5
+print(str)
+print(num)
+print(type(str)) # use type() to check type of variable
+
+# declare variables in one line
+a, b, c = 'Hello' , " I am ", "Simran."
+result = a + b + c
+print(result)
+print("my name is " + c)
+
+fruits = ['apple', 'banana', 'orange']
+x, y, z = fruits # unpacking
+print(x)
+print(y)
+print(z)
