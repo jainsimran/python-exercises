0294a2328b8051a14ca0bdf625ae20e03cb1543c
--replace-all <itsimmi5@gmail.com>
2020-12-27 23:54:15 -0500

    Feat: Added list exercises

---
+++ list.py
@@ -0,0 +1,33 @@
+#replacing the value using index 
+list = ['Apple', 'Banana', 'Watermelon', 'HoneyDew', 'Apple']
+list[1:2] = ["Kiwi", "Mango"]
+print(list)
+
+#adding item in list 
+list.append("Banana")
+list.append("Watermelon")
+print(list)
+
+# looping list 
+for x in list:
+    if x == 'Mango':
+        print(x)
+
+
+# find Kiwi index
+
+# for x in list: 
+#     if x == "Apple":
+#         print(x.index)
+
+# print fruits which have 'a' character
+newList = [x for x in list if "a" in x]
+print(newList)
+
+# print only apples from list 
+appleList = [x for x in list if x == "Apple"]
+print(appleList)
+
+# print only first 3 items in list
+first3Items = [x for x in range(3)]
+print(first3Items)
